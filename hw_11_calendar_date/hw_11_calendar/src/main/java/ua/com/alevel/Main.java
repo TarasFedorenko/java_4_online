d5251ff8bb96f8e22c5b4729e65662d88d9ccbf4
egorfuntusov <funtushan@gmail.com>
2023-02-24 17:56:50 +0200

    add feedback

--- hw_11_calendar_date/hw_11_calendar/src/main/java/ua/com/alevel/Main.java
+++ hw_11_calendar_date/hw_11_calendar/src/main/java/ua/com/alevel/Main.java
@@ -1,12 +1,13 @@
 package ua.com.alevel;
 
 import ua.com.alevel.controller.ControllerCalendar;
+
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-    ControllerCalendar controllerCalendar =new ControllerCalendar();
-    controllerCalendar.run();
 
+    public static void main(String[] args) throws IOException {
+        ControllerCalendar controllerCalendar = new ControllerCalendar();
+        controllerCalendar.run();
     }
 }
